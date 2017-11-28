8ebd6bbbb60d438febae54c343288820771f8af8
Olivier Dias <odias@misterbell.com>
2017-11-28 17:38:23 +0100

    fourth

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -7,19 +7,18 @@ import javax.inject.Inject;
 import play.Configuration;
 import java.lang.System;
 import java.lang.String;
-import java.time.Duration;
-import java.time.temporal.TemporalUnit;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.*;
 
 import play.libs.ws.*;
 import play.libs.Json;
-//import play.mvc.BodyParser.Json;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import play.libs.concurrent.Futures;
+import utils.Url;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -29,70 +28,81 @@ public class HomeController extends Controller {
 
     @Inject Configuration config;
     @Inject WSClient ws;
+
     private String accessToken = "";
     private ArrayList<Transaction> transactionList = new ArrayList<Transaction>();
-    @Inject Futures futures;
-    private final Executor customExecutor = ForkJoinPool.commonPool();
 
 
     public Result index() {
         return ok(views.html.index.render());
     }
 
+
     public CompletionStage<Result> roundedTransactions(String email, String password) {
         transactionList.clear();
         return getAllTransaction(email, password, null);
     }
 
-    private CompletionStage<Result> getAllTransaction(String email, String password, String nextUri) {
+    private CompletionStage<Result> getAllTransaction(String email, String password, String afterParam) {
 
-        WSRequest request = ws.url(((nextUri != null) ? nextUri : "https://sync.bankin.com/v2/transactions"))
+        WSRequest request = ws.url("https://sync.bankin.com/v2/transactions")
                 .setHeader("Bankin-Version", "2016-01-18")
-                .setHeader("Authorization", "Bearer "+accessToken)
+                .setHeader("Authorization", "Bearer " + accessToken)
                 .setQueryParameter("client_id",     config.getString("bankin.clientId"))
                 .setQueryParameter("client_secret", config.getString("bankin.clientSecret"))
-                .setRequestTimeout(5000);
+                .setRequestTimeout(10000);
+        if(afterParam != null){
+            request.setQueryParameter("after", afterParam);
+        }
 
-        return request.get().thenCompose(response -> {
+        return request.get().thenComposeAsync(response -> {
 
             //Bankin response has an unauthorized response, so token is bad, so renew token
             if (response.getStatus() == 401) {
-                    return renewBankinAccessToken(email, password).thenCompose(atResp -> {
+                return renewBankinAccessToken(email, password).thenComposeAsync(atResp -> {
 
-                        //Token has been released, replay transaction request
-                        if (atResp.getStatus() == 200) {
-                            return getAllTransaction(email, password, null);
-                        }
-                        //Token not released, render error with status
-                        return CompletableFuture.supplyAsync(() -> status(atResp.getStatus(), atResp.asJson()).as("application/json"));
-                    });
+                    //Token has been released, replay transaction request
+                    if (atResp.getStatus() == 200) {
+                        return getAllTransaction(email, password, null);
+                    }
+                    //Token not released, render error with status
+                    return CompletableFuture.supplyAsync(() -> status(atResp.getStatus(), atResp.asJson()).as("application/json"));
+                });
             }
 
             JsonNode jsonResp = response.asJson();
             if (jsonResp.has("resources")) {
 
-                    ArrayNode arrayRes = (ArrayNode) jsonResp.withArray("resources");
-                    Iterator<JsonNode> it = arrayRes.iterator();
-                    while (it.hasNext()) {
-                        Transaction resource = Json.fromJson(it.next(), Transaction.class);
-                        transactionList.add(resource);
-                    }
+                System.out.println("add transaction to array "+afterParam);
 
-                    if (jsonResp.has("pagination")) {
-                        String jsonNextUri = jsonResp.findPath("pagination").findPath("next_uri").textValue();
-                        if (jsonNextUri != null) {
-                            System.out.println("papapapapa !!!!!!!!!!!!!!!! (" + transactionList.size() + ") "+jsonNextUri);
-                            try{Thread.sleep(1000);} catch (InterruptedException e) { }
-                            return getAllTransaction(email, password, "https://sync.bankin.com" + jsonNextUri);
+                ArrayNode arrayRes = (ArrayNode) jsonResp.withArray("resources");
+                Iterator<JsonNode> it = arrayRes.iterator();
+                while (it.hasNext()) {
+                    Transaction resource = Json.fromJson(it.next(), Transaction.class);
+                    transactionList.add(resource);
+                }
+
+                if (jsonResp.has("pagination")) {
+                    String jsonNextUri = jsonResp.findPath("pagination").findPath("next_uri").textValue();
+                    if (jsonNextUri != null && jsonNextUri.length() > 0) {
+                        try {
+                            jsonNextUri = Url.getParamValueUrl(new URL("https://sync.bankin.com" + jsonNextUri), "after");
+                        } catch(MalformedURLException e){}
+
+                        //Throttle 10 requests per 1 sec max
+                        try {Thread.sleep(100);} catch (InterruptedException e) {}
+
+                        if(jsonNextUri != null){
+                            return getAllTransaction(email, password, jsonNextUri);
                         }
                     }
-
-                    return CompletableFuture.supplyAsync(() -> ok(Json.toJson(transactionList)).as("application/json"));
+                }
+                return CompletableFuture.supplyAsync(() -> ok(Json.toJson(transactionList)).as("application/json"));
             }
             return CompletableFuture.supplyAsync(() -> status(response.getStatus(), jsonResp).as("application/json"));
-
         });
     }
+
 
     private CompletionStage<WSResponse> renewBankinAccessToken(String email, String password) {
 
---
+++ app/utils/Url.java
@@ -0,0 +1,35 @@
+package utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class Url {
+
+    /*
+    if key exist return value
+    else return null
+     */
+    public static String getParamValueUrl(URL url, String key) {
+
+        try {
+            return Url.splitQuery(url).get(key);
+        } catch (UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+
+    public static Map<String, String> splitQuery(URL url) throws UnsupportedEncodingException {
+        Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+        String query = url.getQuery();
+        String[] pairs = query.split("&");
+        for (String pair : pairs) {
+            int idx = pair.indexOf("=");
+            query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+        }
+        return query_pairs;
+    }
+}
